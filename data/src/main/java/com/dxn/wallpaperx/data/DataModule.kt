125f11afd8dca941156aaf883b15c7e7320c4cff
Divyansh Kushwaha <divyanshdxn@gmail.com>
2022-10-22 01:27:28 +0530

    added OkHttp interceptor for api key

--- data/src/main/java/com/dxn/wallpaperx/data/DataModule.kt
+++ data/src/main/java/com/dxn/wallpaperx/data/DataModule.kt
@@ -8,6 +8,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -26,6 +27,18 @@ object DataModule {
     @Singleton
     fun provideUnsplashApi(): UnsplashApi = Retrofit.Builder().baseUrl("https://api.unsplash.com/")
         .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+        .client(
+            OkHttpClient.Builder()
+                .addInterceptor { chain ->
+                    val req = chain.request()
+                    val newUrl = req.url().newBuilder()
+                        .addQueryParameter("client_id", BuildConfig.UNSPLASH_API_KEY)
+                        .build()
+                    val newReq = req.newBuilder().url(newUrl).build()
+                    chain.proceed(newReq)
+                }
+                .build()
+        )
         .build()
         .create(UnsplashApi::class.java)
 }
