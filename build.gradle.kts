42d0436ea172436cb955e70e6926cd20b9f66c07
Divyansh Kushwaha <thedroiddiv@gmail.com>
2024-01-17 21:43:02 +0530

    chore: use version catalogue for plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ktlint)
 }
 
 allprojects {
