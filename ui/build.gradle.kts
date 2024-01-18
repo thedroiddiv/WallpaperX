c475e74c4a066335971464f5b3c4599305eaaa34
Divyansh Kushwaha <thedroiddiv@gmail.com>
2024-01-18 11:33:51 +0530

    chore: upgrade kotlin version

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.5"
     }
     packagingOptions {
         resources {
